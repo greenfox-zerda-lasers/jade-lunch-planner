ae99ae9f3fad2ad7ce5078b4dae879b58a73a859
Szabo Rihcard <ignocius@gmail.com>
2017-01-16 16:09:27 +0100

    test css added css loaders added

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './AppTest.css';
 
 class App extends Component {
   render() {
-    return <h1>A66 Lunch Planner</h1>
+    return <h1>A66 Lunch Planner</h1>;
   }
 }
 
 
-export default App
+export default App;
