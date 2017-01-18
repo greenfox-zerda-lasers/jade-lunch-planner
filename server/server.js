ec4764cc5fdc4bc5373335f210d8d68490c74c5a
Szabo Rihcard <ignocius@gmail.com>
2017-01-18 14:21:27 +0100

    var changed to const

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-var pg = require('pg');
+const pg = require('pg');
 
 const port = process.env.PORT || 8080;
 
