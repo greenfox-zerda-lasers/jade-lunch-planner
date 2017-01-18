178e4eee1d0beade8d56b4380684ca36c03e1a27
Szabo Rihcard <ignocius@gmail.com>
2017-01-18 17:13:51 +0100

    changes in server and root file

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.listen(port, () => {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-    client.query('SELECT * FROM test_table', function (err, result) {
+    console.log(`client- ${client}, err: ${err}, done:${done}`);
+    client.query('SELECT * FROM test_table', (err, result) => {
       done();
       if (err)
       { console.error(err); response.send("Error " + err); }
