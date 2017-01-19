4f07b5830a338f016a4328ab65e02271667a1108
Endre <endreujhelyi@gmail.com>
2017-01-19 16:39:26 +0100

    set Form

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import Restaurants from '../components/Restaurants';
+
 import './test.scss';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    console.log(event);
+    console.log(`Looking for a restaurant nearby called ${this.state.value}`);
+    event.preventDefault();
+  }
   render() {
     return (
-        <article>
-          <h1>Hello World!!</h1>;
-          <section></section>
-        </article>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Restaurant:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <div>
+          <input type="submit" value="Send" />
+        </div>
+      </form>
+    );
   }
 }
 
