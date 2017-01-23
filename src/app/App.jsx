4dbc4feef4d089d3c90c031cdd40982bebade0de
Endre <endreujhelyi@gmail.com>
2017-01-23 16:19:42 +0100

    design in prog, test, serverset up

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './reset.scss';
 import './App.scss';
 
 
@@ -14,7 +15,7 @@ class App extends Component {
     this.setState({ value: event.target.value });
   }
   handleSubmit(event) {
-    console.log(`Looking for a restauraaant nearby called ${this.state.value}`);
+    console.log(`Looking for a restaurant nearby called ${this.state.value}`);
     this.sendData(this.state);
     event.preventDefault();
   }
@@ -34,27 +35,27 @@ class App extends Component {
   render() {
     return (
       <article className="input-wrapper">
-        <div className="titleBox">
-          <h1>A66</h1>
-          <h2>Lunch Planner</h2>
-        </div>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="search">
+          <img src="../src/imgs/logo.png" alt="logo" />
+          <h1>LUNCH</h1>
+          <label id="location-label" htmlFor="location">Current Lunch <b>Location</b> Is
             <input
-              id="search"
+              id="location"
               type="text"
-              placeholder="search place..."
+              placeholder="..."
               value={this.state.value}
               onChange={this.handleChange}
             />
           </label>
-          <div className="button-wrapper">
-            <input type="submit" value="Send" />
-          </div>
+          <label htmlFor="setTime">Current Lunch <b>Time</b> Is
+            <input
+              id="setTime"
+              type="text"
+              placeholder="..."
+            />
+          </label>
+          <input type="submit" value="Edit to update a plan" />
         </form>
-        <div className="printBox">
-          <h4>Where is {this.state.value}?</h4>
-        </div>
       </article>
     );
   }
