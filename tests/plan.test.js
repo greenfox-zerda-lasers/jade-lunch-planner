a755e63590185f57eb4af4949b583187976aeb68
Szabo Rihcard <ignocius@gmail.com>
2017-02-14 11:12:15 +0100

    tests added package json repaired

--- tests/plan.test.js
+++ tests/plan.test.js
@@ -2,22 +2,35 @@ import plan from '../src/reducers/plan'
 
 describe('Plan', () => {
   it('should give back state', () => {
-	const state = {}
+	const state = {};
 
-	const nextState = plan(state, {})
+	const nextState = plan(state, {});
 
 	expect(state).toEqual(nextState)
-  })
+});
 
   it('should give state after update plan', () => {
-	const state = {}
+	const state = {};
 
 	const action = {
 		type: "UPDATE_PLAN"
-	}
+	};
 
-	const nextState = plan(state, action)
+	const nextState = plan(state, action);
 
 	expect(nextState).toEqual({loading: false})
-  })
-})
+});
+
+  it("should give state after request plan, +loading true", function () {
+  const state = {};
+
+  const action = {
+    type: "REQUEST_PLAN"
+  };
+
+  const nextSate = plan(state, action);
+
+  expect(nextSate).toEqual({loading:true})
+});
+
+});
